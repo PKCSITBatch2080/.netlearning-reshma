662095743a7d6925b9dbb39e35beffc6b7954d38
reshma223 <reshmashrestha478@gmail.com>
2024-02-25 23:05:30 +0545

    readline

--- Greeter/Program.cs
+++ Greeter/Program.cs
@@ -8,7 +8,10 @@ class Program
         Console.WriteLine(fullname);
         string cfullname="hello, reshma shrestha ji";
         Console.WriteLine(cfullname.ToUpper());
-
+        
+         Console.WriteLine("What is your name");
+        var fullname1 =Console.ReadLine();
+        Console.WriteLine($"Your name is {fullname1}.");
 
         
     }

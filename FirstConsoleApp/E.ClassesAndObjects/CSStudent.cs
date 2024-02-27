926eba9e87ba850e74884d89ad35f6e200f819ad
reshma223 <reshmashrestha478@gmail.com>
2024-02-27 08:23:16 +0545

    css

---
+++ FirstConsoleApp/E.ClassesAndObjects/CSStudent.cs
@@ -0,0 +1,13 @@
+//Single Inheritance
+
+
+class CSStudent :student,IGradeable  //Multiple Inheritance
+{
+    public CSStudent(string name,byte rn,string pTitle) : base(name,rn ) 
+    {
+        ProjectTitle=pTitle;
+    }
+    public string ProjectTitle { get; set; }   //prop for property
+    public string InternWork { get; set; }
+    public double GetGrade()=>3.6;
+}
